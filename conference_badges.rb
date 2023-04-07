9f4496ed1017cd7e8fda61ab91f5932610e7452c
Zach <zachlandis91@gmail.com>
2023-04-07 11:33:06 -0600

    commit tree cd1fbdcb8104a427dde425efd3f7eee27d0eb011

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
-# Write your code here.
+require 'pry'
+
+# def badge_maker(name)
+#     "Hello, my name is #{name}."
+# end
+
+# def batch_badge_creator(names)
+#     names.map {|name| "Hello, my name is #{name}."}
+# end
+
+# def assign_rooms(speakers)
+#     speakers.map.with_index(1) do |speaker, room| 
+#         "Hello, #{speaker}! You'll be assigned to room #{room}!"
+#     end
+# end
+
+
+# def printer(attendees)
+#     batch_badge_creator(attendees).each {|badge| puts badge}
+#     assign_rooms(attendees).each {|room_assignment| puts room_assignment}
+# end
+
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room|
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(assignee)
+    batch_badge_creator(assignee).each {|badge| puts badge}
+    assign_rooms(assignee).each {|room_assignment| puts room_assignment}
+end
